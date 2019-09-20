7015dea612f80d17d2225bc93b7ef1ef9c2a9fff
Jan Haltermannjfh@mail.upb.de <jfh@mail.upb.de>
2019-09-20 12:12:02 +0200

    Added a check to avoid to many [||] results
    (if the full segment (or parts that over-approximate i) are set to the information that should be set there, no [||] is returned

--- src/org/sosy_lab/cpachecker/cpa/usageAnalysis/arraySegmentationDomain/transfer/CSegmentationModifier.java
+++ src/org/sosy_lab/cpachecker/cpa/usageAnalysis/arraySegmentationDomain/transfer/CSegmentationModifier.java
@@ -21,22 +21,34 @@ package org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.transf
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import org.sosy_lab.common.log.LogManager;
 import org.sosy_lab.cpachecker.cfa.ast.AExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression;
+import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpression.BinaryOperator;
 import org.sosy_lab.cpachecker.cfa.ast.c.CBinaryExpressionBuilder;
 import org.sosy_lab.cpachecker.cfa.ast.c.CExpression;
 import org.sosy_lab.cpachecker.cfa.ast.c.CIntegerLiteralExpression;
+import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
 import org.sosy_lab.cpachecker.cfa.simplification.ExpressionSimplificationVisitor;
 import org.sosy_lab.cpachecker.cfa.types.MachineModel;
 import org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.ArraySegment;
 import org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.ArraySegmentationState;
 import org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.ExtendedCompletLatticeAbstractState;
 import org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.FinalSegment;
+import org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.formula.FormulaRelation;
 import org.sosy_lab.cpachecker.cpa.usageAnalysis.arraySegmentationDomain.util.ArrayModificationException;
 import org.sosy_lab.cpachecker.cpa.usageAnalysis.instantiationUsage.UsageAnalysisTransferRelation;
 import org.sosy_lab.cpachecker.exceptions.UnrecognizedCodeException;
+import org.sosy_lab.cpachecker.util.Triple;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.PathFormula;
+import org.sosy_lab.cpachecker.util.predicates.pathformula.ctoformula.CtoFormulaConverter;
+import org.sosy_lab.cpachecker.util.predicates.smt.FormulaManagerView;
+import org.sosy_lab.cpachecker.util.predicates.smt.Solver;
+import org.sosy_lab.java_smt.api.BooleanFormula;
+import org.sosy_lab.java_smt.api.Formula;
+import org.sosy_lab.java_smt.api.SolverException;
 
 public class CSegmentationModifier<T extends ExtendedCompletLatticeAbstractState<T>> {
 
@@ -66,14 +78,30 @@ public class CSegmentationModifier<T extends ExtendedCompletLatticeAbstractState
     // Check, if the expression used to access the array element is present in the current state
     int pos = state.getSegBoundContainingExpr(pIndex);
     if (pos < 0) {
-      String errorMsg =
-          UsageAnalysisTransferRelation.PREFIX
-              + "Cannot create a usage sincethe variable "
-              + pIndex.toASTString()
-              + " is not present in the segmentation, hence the error symbol is returned. Current State is: "
-              + state.toDOTLabel();
-      logger.log(Level.FINE, errorMsg);
-      throw new ArrayModificationException(errorMsg);
+      // Check, if we can compute an interval for the index being not present using the assumption
+      // that 0 <= i and i <= SIZE and check, if the analysis information that hold be stored in the
+      // interval is already present in the over-approximated interval computed for i
+      boolean error = false;
+      int lowerBound = computeLowerBound(state, pIndex);
+      int upperBound = computeUpperBound(state, pIndex);
+      for (int i = lowerBound; i < upperBound; i++) {
+        if (!state.getSegments().get(i).getAnalysisInformation().equals(pAnalysisInfo)) {
+          error = true;
+        }
+      }
+      if (!error && upperBound > lowerBound) {
+        return state;
+      } else {
+
+        String errorMsg =
+            UsageAnalysisTransferRelation.PREFIX
+                + "Cannot create a usage since the variable "
+                + pIndex.toASTString()
+                + " is not present in the segmentation, hence the error symbol is returned. Current State is: "
+                + state.toDOTLabel();
+        logger.log(Level.FINE, errorMsg);
+        throw new ArrayModificationException(errorMsg);
+      }
     } else {
       // Create a new segment after the segment containing the expression to access the array
       // elements and mark this as used
@@ -120,6 +148,79 @@ public class CSegmentationModifier<T extends ExtendedCompletLatticeAbstractState
           pAnalysisInfo,
           pNewSegmentIsPotentiallyEmpty);
     }
+  }
+
+  private int computeUpperBound(ArraySegmentationState<T> pState, CExpression pIndex) {
+    int index = pState.getSegments().size() - 1;
+    BooleanFormula formula = pState.getPathFormula().getPathFormula().getFormula();
+    FormulaRelation pr = pState.getPathFormula().getPr();
+    Solver solver = pr.getSolver();
+
+    // Compute for each segment present in the segmentation, if for any expression e it holds that:
+    // pINdex >=e is SAT, (>= since e is the upper bound of the interval not included, hence
+    // everything up to e need to fulfill the condition
+    for (int i = pState.getSegments().size() - 1; i >= 0; i--) {
+      ArraySegment<T> sb = pState.getSegments().get(i);
+      for (AExpression e : sb.getSegmentBound()) {
+        Optional<BooleanFormula> smaller =
+            getBooleanFormula(
+                pIndex,
+                (CExpression) e,
+                BinaryOperator.GREATER_EQUAL,
+                pr.getConverter(),
+                pState.getPathFormula().getPathFormula(),
+                null,
+                pr.getFormulaManager());
+        if (smaller.isPresent()) {
+          try {
+            if (solver.implies(formula, smaller.get())) {
+              index = i;
+              break;
+            }
+          } catch (SolverException | InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+          }
+        }
+      }
+    }
+    return index;
+  }
+
+  private int computeLowerBound(ArraySegmentationState<T> pState, CExpression pIndex) {
+    int index = 0;
+    BooleanFormula formula = pState.getPathFormula().getPathFormula().getFormula();
+    FormulaRelation pr = pState.getPathFormula().getPr();
+    Solver solver = pr.getSolver();
+
+    // Compute for each segment present in the segmentation, if for any expression e it holds that:
+    // pINdex <= e is SAT (same arguemtn as above)
+    for (int i = 1; i < pState.getSegments().size(); i++) {
+      ArraySegment<T> sb = pState.getSegments().get(i);
+      for (AExpression e : sb.getSegmentBound()) {
+        Optional<BooleanFormula> smaller =
+            getBooleanFormula(
+                pIndex,
+                (CExpression) e,
+                BinaryOperator.LESS_EQUAL,
+                pr.getConverter(),
+                pState.getPathFormula().getPathFormula(),
+                null,
+                pr.getFormulaManager());
+        if (smaller.isPresent()) {
+          try {
+            if (solver.implies(formula, smaller.get())) {
+              index = i;
+              break;
+            }
+          } catch (SolverException | InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+          }
+        }
+      }
+    }
+    return index;
   }
 
   public ArraySegmentationState<T> storeAnalysisInformationAtIndexWithoutAddingBounds(
@@ -172,6 +273,36 @@ public class CSegmentationModifier<T extends ExtendedCompletLatticeAbstractState
 
       return state;
     }
+
+  }
+
+  private Optional<BooleanFormula> getBooleanFormula(
+      CExpression pExpr1,
+      CExpression pExpr2,
+      BinaryOperator pOp,
+      CtoFormulaConverter converter,
+      PathFormula pPathFormula,
+      CFAEdge pCfaEdge,
+      FormulaManagerView manager) {
+
+    CBinaryExpression sizeGEqZero;
+    try {
+      sizeGEqZero = builder.buildBinaryExpression(pExpr1, pExpr2, pOp);
+
+      Formula ifThenElseFormulaGEQ =
+          converter.buildTermFromPathFormula(pPathFormula, sizeGEqZero, pCfaEdge);
+      Optional<Triple<BooleanFormula, Formula, Formula>> bfGEQOptional =
+          manager.splitIfThenElse(ifThenElseFormulaGEQ);
+      if (bfGEQOptional.isPresent()) {
+        return Optional.of(bfGEQOptional.get().getFirst());
+      }
+    } catch (UnrecognizedCodeException e) {
+      e.printStackTrace();
+      return Optional.empty();
+    } catch (Throwable e) {
+      System.out.println();
+    }
+    return Optional.empty();
 
   }
 }
