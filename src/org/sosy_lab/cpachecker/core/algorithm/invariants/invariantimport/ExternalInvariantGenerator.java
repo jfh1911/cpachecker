5cfbee8d988f15cfc3af0c23af3a27f2f3000ca0
Jan <jan.haltermann@googlemail.com>
2020-01-02 09:48:41 +0100

    Created a working version of the invariant generation
    Now, there is a configuration that calls an external invariant generation tool befoer executing bmc.
    TODO: include it in the full pipeline!

---
+++ src/org/sosy_lab/cpachecker/core/algorithm/invariants/invariantimport/ExternalInvariantGenerator.java
@@ -0,0 +1,60 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2019  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.sosy_lab.cpachecker.core.algorithm.invariants.invariantimport;
+
+import java.util.List;
+import java.util.Set;
+import org.sosy_lab.common.ShutdownNotifier;
+import org.sosy_lab.common.configuration.Configuration;
+import org.sosy_lab.common.log.LogManager;
+import org.sosy_lab.cpachecker.cfa.CFA;
+import org.sosy_lab.cpachecker.cfa.model.CFANode;
+import org.sosy_lab.cpachecker.core.Specification;
+import org.sosy_lab.cpachecker.core.algorithm.bmc.candidateinvariants.CandidateInvariant;
+import org.sosy_lab.cpachecker.exceptions.CPAException;
+
+public interface ExternalInvariantGenerator {
+
+  /**
+   *
+   * @param pCfa of the program
+   * @param targetNodesToGenerateFor A List of nodes for that a invariant should be generated
+   * @return A set of candidate invariants generated and verified
+   * @throws CPAException If the CFA contains more than one source file or to wrap different
+   *         exceptions
+   */
+  Set<CandidateInvariant> generateInvariant(
+      CFA pCfa,
+      List<CFANode> pTargetNodesToGenerateFor,
+      Specification pSpecification,
+      LogManager pLogger,
+      ShutdownNotifier pShutdownManager,
+      Configuration pConfig)
+      throws CPAException;
+
+  static ExternalInvariantGenerator getInstance(ExternalInvariantGenerators instance) {
+    switch (instance) {
+      case SEAHORN:
+      default:
+        return new SeahornInvariantGenerator();
+
+    }
+  }
+}
