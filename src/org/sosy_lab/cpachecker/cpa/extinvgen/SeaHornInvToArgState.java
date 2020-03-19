32b9ddb9130cd12f3c2929ac552327f53497de81
Jan Haltermann <jfh@mail.upb.de>
2020-03-19 12:25:39 +0100

    fixed hashcode

--- src/org/sosy_lab/cpachecker/cpa/extinvgen/SeaHornInvToArgState.java
+++ src/org/sosy_lab/cpachecker/cpa/extinvgen/SeaHornInvToArgState.java
@@ -66,7 +66,7 @@ public class SeaHornInvToArgState
 
   @Override
   public int hashCode() {
-    return Objects.hash(defaultValue, globalInvMap, location);
+    return Objects.hash(location);
   }
 
   @Override
